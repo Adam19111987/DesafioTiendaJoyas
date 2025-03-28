74939e50ab931ca6d02707fade194463ed311e36
cristian Di d Silv0 <a.diaz.1987.08@hotmail.com>
2025-03-28 19:30:37 -0300

    trabajo finalizado

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 import {envs} from "./config/envs.js";
 import express from "express"
 import joyasRouter from "./router/joyas.router.js"
+import { logger } from "logger-express";
 
+const loggerOption = {
+    logToFile: true, // If you need to log information to a file
+    colorize: true, // enable console colors
+    infoColor: "magenta", // set a color for information messages
+    errorColor: "red", // set a color for error messages:
+  };
 
 const app = express();
 const ports = envs.port
 app.listen(ports, () => { console.log(`Server on! in host : ${ports}`)})
 
 app.use(express.json()) 
-
+app.use(logger(loggerOption));
 app.use("/api", joyasRouter)
